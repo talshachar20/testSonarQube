4d72332f405996743b13921915e77af6fad79796
Tal Shachar <tal.shachar@mondiamedia.com>
2019-02-15 16:13:23 +0100

    first commit

---
+++ src/main/java/com/modiamedia/qa/service/security_api_rest_tests/test/App.java
@@ -0,0 +1,13 @@
+package com.modiamedia.qa.service.security_api_rest_tests.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
